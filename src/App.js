50063921bdaad467b042f77de4d08f018fbd9186
Stephanie Raymos <stephanieraymos@gmail.com>
2020-06-22 19:37:20 -0700

    Getting components and router started

--- src/App.js
+++ src/App.js
@@ -4,9 +4,29 @@ import './App.css';
 export class App extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+<div className="demo-big-content">
+    <Layout>
+        <Header title="Title" scroll>
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+        </Content>
+    </Layout>
+</div>
     );
   }
 }
