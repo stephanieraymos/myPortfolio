0cf4ac26507928b8c5f4126179fb50120a20dcb1
Stephanie Raymos <stephanieraymos@gmail.com>
2020-06-24 13:56:06 -0700

    Added avatar and some text to Home

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -4,9 +4,18 @@ import { Grid, Cell } from 'react-mdl';
 export class Home extends Component {
   render() {
     return (
-      <div style={{width: '100%', margin: 'auto'}}>
-        <Grid className="landing-grid">
-          <Cell col={12}></Cell>
+      <div style={{ width: '100%', margin: 'auto' }}>
+        <Grid className="home-grid">
+          <Cell col={12}>
+            <img
+              src="https://cdn5.vectorstock.com/i/1000x1000/72/74/female-avatar-profile-icon-round-woman-face-vector-18307274.jpg"
+              alt="Avatar"
+              className="avatar"
+            />
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+            </div>
+          </Cell>
 
         </Grid>
       </div>
