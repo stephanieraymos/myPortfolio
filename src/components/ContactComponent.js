cf50ce47d9ffb38692ac6909c1503facdd24ea1c
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-14 12:48:24 -0700

    Contact avatar edit

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { List, ListItem, ListItemContent } from 'react-mdl';
 import { Container, Row, Col } from 'react-bootstrap';
+import stephanie from './img/stephanie.jpg'
 
 class Contact extends Component {
   render() {
@@ -11,9 +12,9 @@ class Contact extends Component {
             <Col>
               <h2>Stephanie Raymos</h2>
               <img
-                src="https://lh3.googleusercontent.com/proxy/QkmSzq51pwj6A6UsYKXQClOctohtdiaKTYIH2s-KIGAnKnMY_t2-TCN5AJ2bAWyrvAqyjn26qwnCP0K0p9Fgvan4TY9Tm3-OAUaVS4Gyc0kv6g9_zmbYIVGg8w0uLLfiVjjVEEU_ohh5QpBlU4sUPatdXl0Z9otlF4l5nSrBW-ksPRROu_suLA"
+                src={stephanie}
                 alt="avatar"
-                style={{ height: '250px' }}
+                style={{ height: '300px', width: '225px', objectFit: 'contain' }}
               />
               <p style={{ width: '70%', margin: 'auto', paddingTop: '1em' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </Col>
@@ -24,7 +25,7 @@ class Contact extends Component {
               <div className="contact-list">
                 <List>
                   <ListItem>
-                    <ListItemContent style={{ fontSize: '30px', fontFamily: 'Oswald', justifyContent: 'center'}}>
+                    <ListItemContent style={{ fontSize: '30px', fontFamily: 'Oswald', justifyContent: 'center' }}>
                       <i className="fa fa-envelope" aria-hidden="true" />
                     stephanieraymos@gmail.com
                   </ListItemContent>
